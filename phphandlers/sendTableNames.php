2b25f846f0ff174957cd13f0220ce653ee16f38c
SabbaKilam <abbas229@gmail.com>
2014-09-24 08:31:50 -0700

    First Commit for qndsql for PhoneGap

---
+++ phphandlers/sendTableNames.php
@@ -0,0 +1,32 @@
+<?php
+ini_set('display_errors','off');
+session_start();
+echo $_SESSION['filename'];
+//======================================
+$_SESSION['id'] = session_id();
+$_SESSION['topFolderPath'] = "C:/xampp/htdocs/dbfiles/";
+$_SESSION['targetFolderPath'] = $_SESSION['topFolderPath'] . $_SESSION['id'] . "/";
+$_SESSION['fullFilePath'] = $_SESSION['targetFolderPath'] . $_SESSION['filename'];
+//======================================
+include_once "C:/xampp/htdocs/adodb/adodb.inc.php";
+$fullFilePath = $_SESSION['fullFilePath'];
+if (!file_exists($fullFilePath)) {
+    die("Could not find database file");
+}
+
+$db = ADONewConnection('access');
+$db->debug = true;
+$dsn = "DRIVER={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=" . $fullFilePath ."; Uid=; Pwd=;";
+$db->Connect($dsn);
+$aryTables = $db->MetaTables();
+$tableCount = count($aryTables);
+
+$strTables = ""; 
+for($i = 0; $i <$tableCount ; $i++){
+    $strTables .= ( ( $i < ($tableCount -1) ) ? ( $aryTables[$i] . '``') : $aryTables[$i] );
+}
+$_SESSION['tables'] = $strTables;
+$tableOrTables = ($tableCount === 1)?" table":" tables";
+exit($strTables);
+//-----------------------------
+?>
